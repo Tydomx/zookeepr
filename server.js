a99bccee87e580cf41146a4ec2fe06c0339be9b8
Tydomx <michaelthai16@gmail.com>
2022-05-25 11:13:14 -0500

    added POST route, and allowing it to be validated properly

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 // packages express
 const express = require('express');
+const fs = require('fs');
+// provides utilities for working w file and directory paths
+const path = require('path');
 // setting environment variable 'process.env.PORT' use this port or default to 3001
 const PORT = process.env.PORT || 3001;
 // instantiate the server
 const app = express();
+
+// parse incoming string or array data (built-in express method - taking incoming POST data and converts it to key/value pairings that can be accessed in req.body)
+// extended:true - informs our server that there may be sub-array data nested in it as well, so it needs to look as deep into POST data as possible to parse all data correctly
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data - (takes incoming POST data in form of JSON and prases it into the req.body)
+app.use(express.json());
+// both middleware functions need to be set up every time you create a server that's looking for POST data
 const { animals } = require('./data/animals.json');
 
 
@@ -51,6 +61,35 @@ function findById(id, animalsArray) {
     return result;
 }
 
+// function that accepts POST route's req.body value and array we want to add data to.
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    // when we POST new animal, we'll add it to imported 'animals' array from JSON file
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    // return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 // adding the GET route 
 app.get('/api/animals', (req, res) => {
     // this accesses the query property on req object
@@ -72,6 +111,25 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+// defining a route that listens to POST requests, not GET requests.
+// POST requests differe from GET requests in that they rep action of client requesting server to accept data rather than vice versa
+app.post('/api/animals', (req, res) => {
+    // set id based on what next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+
+        res.json(animal);
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
